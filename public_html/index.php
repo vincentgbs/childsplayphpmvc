9bc127e47d2cd3d91b408ff7bedb8af51b0ed584
Vincent Hu <vincent.gbs11@gmail.com>
2015-04-27 18:27:15 -0400

    Added CDNs as comments

--- public_html/index.php
+++ public_html/index.php
@@ -2,11 +2,17 @@
 <head>
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
     <meta content="utf-8" http-equiv="encoding">
+    <!-- <link rel="stylesheet" href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'/> -->
     <link rel="stylesheet" href='css/bootstrap.css'/>
+<!-- <script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script> -->
     <script type="text/javascript" src='js/jquery.js'></script>
+    <!-- <script type="text/javascript" src=''></script> -->
     <script type="text/javascript" src='js/bootstrap.js'></script>
+<!-- <script type="text/javascript" src='https://code.jquery.com/ui/1.11.4/jquery-ui.min.js'></script>-->
     <script type="text/javascript" src='js/jqueryui.js'></script>
+    <!-- <link rel="stylesheet" href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'/> -->
     <link rel="stylesheet" href='css/jqueryui.css'/>
+    <!-- <script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.js'></script> -->
     <script type="text/javascript" src='js/jquery.validate.js'></script>
 </head>
 <?php
